c3dc58308d9b316075f122293669476c4de73f8c
mrLuisFer <mrluisfeer@gmail.com>
2021-07-22 10:42:21 -0500

    fix: adding a return for test

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	cli "github.com/mrLuisFer/go-basic-cli/src/cli"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	cli()
 }
--- src/cli/cli.go
+++ src/cli/cli.go
@@ -2,6 +2,7 @@ package cli
 
 import "fmt"
 
-func Cli() {
+func Cli() string {
 	fmt.Print("Working the CLI file")
+	return "Works?"
 }
