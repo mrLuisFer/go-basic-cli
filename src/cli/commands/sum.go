b875744fdb16ca7e428b828ee80bd714cd6341ff
mrLuisFer <mrluisfeer@gmail.com>
2021-07-24 09:40:09 -0500

    feat: handling errors when entering numbers

--- src/cli/commands/sum.go
+++ src/cli/commands/sum.go
@@ -13,6 +13,7 @@ func Sum(){
 
 	var numb1 int
 	var numb2 int
+	var error bool
 
 	colors.Info("Insert a number: ", false)
 	fmt.Scanln(&numb1)
@@ -20,12 +21,19 @@ func Sum(){
 	colors.Info("Insert second number: ", false)
 	fmt.Scanln(&numb2)
 
-	var typeNumber1 = reflect.TypeOf(numb1).Kind()
-	var typeNumber2 = reflect.TypeOf(numb2).Kind()
+	if (numb1 <= 0 && numb2 <= 0) {
+		error = true
+	}
+	
+	typeNumber1 := reflect.TypeOf(numb1).Kind()
+	typeNumber2 := reflect.TypeOf(numb2).Kind()
 
-	if (typeNumber1 == reflect.Int && typeNumber2 == reflect.Int) {
+	if (typeNumber1 == reflect.Int && typeNumber2 == reflect.Int && !error) {
 		result := numb1 + numb2
 		colors.Succes("The result is:", false)
 		fmt.Printf(" %v \n", result)
+	} else {
+		colors.Error("Insert a valid number!", false)
+		error = true
 	}
 }
