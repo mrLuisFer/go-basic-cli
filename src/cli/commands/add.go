db40cf635c4e14876785954754cc649984f63b63
mrLuisFer <mrluisfeer@gmail.com>
2021-07-24 09:16:32 -0500

    feat: Add logic for the sum command

--- src/cli/commands/add.go
+++ src/cli/commands/add.go
@@ -2,8 +2,30 @@ package commands
 
 import (
 	"fmt"
+
+	"reflect"
+
+	colors "github.com/mrLuisFer/go-basic-cli/src/utils/colors"
 )
 
 func Sum(){
-	fmt.Print("Sum command")
+	colors.Warn("Insert two numbers!", true)
+
+	var numb1 int
+	var numb2 int
+
+	colors.Info("Insert a number: ", false)
+	fmt.Scanln(&numb1)
+
+	colors.Info("Insert second number: ", false)
+	fmt.Scanln(&numb2)
+
+	var typeNumber1 = reflect.TypeOf(numb1).Kind()
+	var typeNumber2 = reflect.TypeOf(numb2).Kind()
+
+	if (typeNumber1 == reflect.Int && typeNumber2 == reflect.Int) {
+		result := numb1 + numb2
+		colors.Succes("The result is:", false)
+		fmt.Printf(" %v", result)
+	}
 }
